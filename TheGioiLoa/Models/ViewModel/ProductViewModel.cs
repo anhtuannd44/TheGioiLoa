aacfa62438de2f5e358bceb40bb50981901591e8
Nguyễn Dương Anh Tuấn <anhtuan.nd44@gmail.com>
2020-02-20 09:42:45 +0700

    opload

--- TheGioiLoa/Models/ViewModel/ProductViewModel.cs
+++ TheGioiLoa/Models/ViewModel/ProductViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace TheGioiLoa.Models.ViewModel
 {
@@ -18,17 +19,37 @@ namespace TheGioiLoa.Models.ViewModel
         [Column(TypeName = "ntext")]
         public string Description { get; set; }
 
+        [Display(Name = "Thương hiệu")]
+        public int? BrandId { get; set; }
+
         [Display(Name = "Giá bán")]
         public double? Price { get; set; }
 
         [Display(Name = "Giá niêm yết")]
         public double? ListedPrice { get; set; }
 
+        [AllowHtml]
+        [Display(Name = "Đặc điểm nổi bật")]
+        [Column(TypeName = "ntext")]
+        public string Characteristics { get; set; }
+
+        [AllowHtml]
+        [Display(Name = "Thông số kỹ thuật")]
+        [Column(TypeName = "ntext")]
+        public string Details { get; set; }
+
+        [AllowHtml]
+        [Display(Name = "Khuyến mãi")]
+        public string Promotion { get; set; }
+
+        [AllowHtml]
+        [Display(Name = "Video")]
+        public string Video { get; set; }
+
         [Display(Name = "Trạng thái sản phẩm")]
         public int Status { get; set; }
 
-        [Display(Name = "Thương hiệu")]
-        public int? BrandId { get; set; }
+        public string Tag { get; set; }
     }
 
     public class CreateProductActionViewModel
